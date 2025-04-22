6e549cc0a50a1f04cc3496b488d4d58f41af6a96
AndrewNTN <70861389+AndrewNTN@users.noreply.github.com>
2025-04-22 12:08:34 -0400

    client connect

--- Reliable-UDP-Chat/server_1.py
+++ Reliable-UDP-Chat/server_1.py
@@ -11,6 +11,7 @@ class Server:
     '''
     This is the main Server Class. You will  write Server code inside this class.
     '''
+
     def __init__(self, dest, port, window):
         self.server_addr = dest
         self.server_port = port
@@ -19,13 +20,47 @@ class Server:
         self.sock.settimeout(None)
         self.sock.bind((self.server_addr, self.server_port))
 
+        self.clients = {}  # conn_socket : Username
+
     def start(self):
         '''
         Main loop.
         continue receiving messages from Clients and processing it.
-
         '''
-        raise NotImplementedError # remove it once u start your implementation
+
+        while True:
+            try:
+                # wait for a client connection
+                conn_socket, addr = self.sock.accept()
+
+                # get message
+                message = conn_socket.recv(4096).decode()
+
+                # handle message
+                if message:
+                    self.handle_message(message, conn_socket)
+
+            except Exception as e:
+                print(f"Error: {e}")
+
+    def handle_message(self, message, conn_socket):
+        msg_type, seqno, data, checksum = util.parse_packet(message)
+
+        if data == "disconnect":
+            print(f"disconnected: {self.clients[conn_socket]}")
+            del self.clients[conn_socket]
+
+    def add_client(self, client_name, conn_socket):
+        if len(self.clients) >= util.MAX_NUM_CLIENTS:
+            # send client disconnected: full
+            pass
+
+        if client_name in self.clients.values():
+            # send client disconnected: username not available
+            pass
+
+        self.clients[client_name] = conn_socket
+
 
 # Do not change below part of code
 
@@ -40,9 +75,10 @@ if __name__ == "__main__":
         print("-w WINDOW | --window=WINDOW The window size, default is 3")
         print("-h | --help Print this help")
 
+
     try:
         OPTS, ARGS = getopt.getopt(sys.argv[1:],
-                                   "p:a:w", ["port=", "address=","window="])
+                                   "p:a:w", ["port=", "address=", "window="])
     except getopt.GetoptError:
         helper()
         exit()
@@ -59,7 +95,7 @@ if __name__ == "__main__":
         elif o in ("-w", "--window="):
             WINDOW = a
 
-    SERVER = Server(DEST, PORT,WINDOW)
+    SERVER = Server(DEST, PORT, WINDOW)
     try:
         SERVER.start()
     except (KeyboardInterrupt, SystemExit):
